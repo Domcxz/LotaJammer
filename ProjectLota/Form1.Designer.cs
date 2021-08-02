10722ef978a34c96d9c733c76c26a13c9bfde5fa
Domcxz <Domcxzs@gmail.com>
2021-08-02 15:04:54 +0300

    Initial commit

---
+++ ProjectLota/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace ProjectLota
+{
+    partial class LotaJammer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LotaJammer));
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(12, 278);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(187, 75);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.SlateBlue;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(12, 38);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(174, 204);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Font = new System.Drawing.Font("Comic Sans MS", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(205, 278);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(167, 75);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Reload";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.SlateBlue;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(360, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 249);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(360, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.BackColor = System.Drawing.Color.SlateBlue;
+            this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox2.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(192, 38);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(180, 204);
+            this.listBox2.TabIndex = 5;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(314, 38);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(58, 32);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button5.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(340, 76);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(32, 32);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "-";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // LotaJammer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(384, 371);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "LotaJammer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LotaJammer";
+            this.Load += new System.EventHandler(this.LotaJammer_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ ProjectLota/Form1.cs
@@ -0,0 +1,242 @@
+﻿using ProjectLota.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectLota
+{
+    public partial class LotaJammer : Form
+    {
+        public string cFile = "config.lota";
+        public LotaJammer()
+        {
+            InitializeComponent();
+        }
+
+        List<string> items = new List<string>();
+        List<string> stwingawway = new List<string>();
+        private void LotaJammer_Load(object sender, EventArgs e)
+        {
+            //Thread thrd = new Thread(ListProcesses) { IsBackground = true };
+            LoadSettings();
+            ListProcesses();
+            items.AddRange(stwingawway);
+            label1.Text = "";
+            foreach (string str in items)
+            {
+                listBox1.Items.Add(str);
+            }
+        }
+
+        Process[] processCollection;
+        private void ListProcesses()
+        {
+            processCollection = Process.GetProcesses();
+            foreach (Process p in processCollection)
+            {
+                listBox1.Items.Add(p.ProcessName);
+                stwingawway.Add(p.ProcessName);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text=null;
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            foreach (string str in items)
+            {
+                if (str.StartsWith(textBox1.Text, StringComparison.CurrentCultureIgnoreCase))
+                {
+                    listBox1.Items.Add(str);
+                }
+            }
+        }
+        public List<string> selectedItemsA=new List<string>();
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //usual stuff
+            var selectedItem = listBox1.SelectedItem;
+            selectedItemsA.Add(selectedItem.ToString());
+            listBox2.Items.Add(selectedItem);
+            //listBox2.Items.Clear();
+            SaveSettings();
+        }
+        public void SaveSettings(){
+            List<string> s = new List<string>();
+            foreach (string asd in listBox2.Items)
+            {
+                s.Add(asd);
+            }
+            if (!File.Exists(cFile))
+            {
+                var file=File.Create(cFile);
+                file.Close();
+            }
+            File.WriteAllLines(cFile, s);
+        }
+        public void LoadSettings()
+        {
+            List<string> s = new List<string>();
+            string[] ss;
+            if (File.Exists(cFile))
+            {
+                var sss = File.ReadAllLines(cFile);
+                s.Clear();
+                foreach (string ssss in sss){
+                    s.Add(ssss);
+                }
+                ss = s.ToArray();
+                listBox2.Items.Clear();
+                foreach (string sssss in ss){
+                    listBox2.Items.Add(sssss);
+                }
+                selectedItemsA=s;
+            }
+
+        }
+        //Kill sellected process
+        public bool doLoopKill=false;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            doLoopKill = !doLoopKill;
+            //Change button color based on doLoopKill value
+            if (doLoopKill)
+            {
+                button1.ForeColor = Color.Green;
+                cIcon(Color.Green);
+                Bitmap sB = new Bitmap(pictureBox1.Image);
+                this.Icon = Icon.FromHandle(sB.GetHicon());
+                sB.Dispose();
+            }
+            else
+            {
+                button1.ForeColor = Color.Red;
+                cIcon(Color.Red);
+                Bitmap sB = new Bitmap(pictureBox1.Image);
+                this.Icon = Icon.FromHandle(sB.GetHicon());
+                sB.Dispose();
+            }
+            Thread thrd = new Thread(noFreezeKillLoop) { IsBackground = true };
+            if (doLoopKill){
+                if (selectedItemsA.Count < 0)
+                {
+                    label1.Text = "Processes are getting jammed :))";
+                }
+                else
+                {
+                    label1.Text = "Process is getting jammed :)";
+                }
+                thrd.Start();
+            }
+            else{
+                thrd.Abort();
+            }
+        }
+        public void noFreezeKillLoop(){
+            //Kill sellected process
+            while (doLoopKill)
+            {
+                Process[] pp;
+                pp = Process.GetProcesses();
+                foreach (Process p in pp)
+                {
+                    foreach (string Name in selectedItemsA){
+                        if (p.ProcessName == Name)
+                        {
+                            //Debug.WriteLine(p.ProcessName, p.Id);
+                            p.Kill();
+                            //Invoke(new Action(() =>
+                            //{
+
+                            //}));
+                        }
+                    }
+                }
+                Task.Delay(100).Wait();
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Set window icon to grey (Default)
+            cIcon(Color.Gray);
+            Bitmap sB = new Bitmap(pictureBox1.Image);
+            this.Icon = Icon.FromHandle(sB.GetHicon());
+            sB.Dispose();
+            //Set button1.ForeGround color to black
+            button1.ForeColor = Color.Black;
+            doLoopKill = false;
+            label1.Text = "";
+            //Reload ListBox1
+            listBox1.Items.Clear();
+            Process[] pr;
+            pr = Process.GetProcesses();
+            List<string> s = new List<string>();
+            foreach (Process word in pr)
+            {
+                listBox1.Items.Add(word.ProcessName);
+                s.Add(word.ProcessName);
+            }
+
+            //Reload Filters
+            items.Clear();
+            items.AddRange(s);
+            foreach (string str in items)
+            {
+                listBox1.Items.Add(str);
+            }
+            listBox1.Items.Clear();
+
+            foreach (string str in items)
+            {
+                if (str.StartsWith(textBox1.Text, StringComparison.CurrentCultureIgnoreCase))
+                {
+                    listBox1.Items.Add(str);
+                }
+            }
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedItem!=null){
+                listBox2.Items.Remove(listBox2.SelectedItem);
+                listBox2.SelectedItem = null;
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            File.WriteAllText(cFile, "");
+        }
+        PictureBox pictureBox1 = new PictureBox();
+        public void cIcon(Color brush)
+        {
+            pictureBox1.Size = new Size(400, 400);
+            Bitmap flag = new Bitmap(400, 400);
+            Graphics flagGraphics = Graphics.FromImage(flag);
+            Brush clr = new SolidBrush(brush);
+            flagGraphics.FillRectangle(clr, 0, 0, 400, 400);
+            pictureBox1.Image = flag;
+        }
+    }
+}
